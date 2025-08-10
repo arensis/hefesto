a32471f15b97ab7903216cdb90de07914b24192f
Alfonso Madrid <alfonso.madrid.dev@gmail.com>
2025-08-10 15:55:37 +0200

    test with connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,18 @@ import { StationsModule } from './stations/stations.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: `mongodb://${configService.get(
-          'DATABASE_HOST',
-        )}:${configService.get('DATABASE_PORT')}/${configService.get(
-          'DATABASE_NAME',
-        )}`,
-      }),
-      inject: [ConfigService],
-    }),
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) => ({
+    //     uri: `mongodb://${configService.get(
+    //       'DATABASE_HOST',
+    //     )}:${configService.get('DATABASE_PORT')}/${configService.get(
+    //       'DATABASE_NAME',
+    //     )}`,
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    MongooseModule.forRoot('mongodb://192.168.1.145:3000/mongo-kairos'),
     StationsModule,
   ],
   providers: [],
