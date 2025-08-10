27dced589824d35eadc4de8fa86ea09cec707a58
Alfonso Madrid <alfonso.madrid.dev@gmail.com>
2025-08-10 16:06:34 +0200

    test connection 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { StationsModule } from './stations/stations.module';
     //   }),
     //   inject: [ConfigService],
     // }),
-    MongooseModule.forRoot('mongodb://127.0.0.1:3000/mongo-kairos'),
+    MongooseModule.forRoot('mongodb://127.0.0.1/mongo-kairos'),
     StationsModule,
   ],
   providers: [],
